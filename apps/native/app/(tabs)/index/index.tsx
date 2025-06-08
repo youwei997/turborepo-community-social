8a1738ba02c6efdc13b50e5b2344205b1630d466
youwei <1690312648@qq.com>
2025-06-08 19:04:08 +0800

    feat(tabs): 更新首页功能
    - 重构首页组件，命名为 IndexScreen
    - 集成 Convex 数据库，获取和展示话题列表
    - 添加创建话题功能
    - 优化页面样式，使用 FlatList 和 Button 组件

--- apps/native/app/(tabs)/index/index.tsx
+++ apps/native/app/(tabs)/index/index.tsx
@@ -1,15 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { api } from '@repo/convex/_generated/api';
+import { useMutation, useQuery } from 'convex/react';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
-const HomeScreen: React.FC = () => {
+const IndexScreen: React.FC = () => {
+  const topics = useQuery(api.topics.get);
+  console.log(topics);
+
+  const createTopic = useMutation(api.topics.create);
+
   return (
     <View style={styles.container}>
+      <FlatList
+        data={topics}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.desc}>{item.description}</Text>
+          </View>
+        )}
+      />
+      <Button
+        title="按钮"
+        onPress={() => {
+          console.log('点击了按钮');
+          createTopic({
+            title: '测试标题',
+          });
+        }}
+      ></Button>
       <Text style={styles.title}>欢迎来到首页</Text>
       <Text>这是一个基础的 React Native 页面。</Text>
     </View>
   );
 };
 
-export default HomeScreen;
+export default IndexScreen;
 
 // 样式定义
 const styles = StyleSheet.create({
@@ -25,5 +51,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 16,
     color: '#333',
+  },
+  item: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  desc: {
+    fontSize: 14,
+    color: '#666',
+    marginTop: 4,
   },
 });
--- packages/convex/topics.ts
+++ packages/convex/topics.ts
@@ -1,8 +1,31 @@
-import { query } from "./_generated/server";
+import { v } from 'convex/values';
+import { mutation, query } from './_generated/server';
 
 export const get = query({
   args: {},
-  handler: async (ctx) => {
-    return await ctx.db.query("topics").collect();
+  handler: async ctx => {
+    return await ctx.db.query('topics').collect();
+  },
+});
+
+export const create = mutation({
+  args: { title: v.string() },
+  handler: async (ctx, args) => {
+    const now = Date.now();
+    const topic = await ctx.db.insert('topics', {
+      title: args.title,
+      description: 'description',
+      coverImage: '',
+      creatorId: '',
+      creatorName: '',
+      creatorAvatar: '',
+      followersCount: 0,
+      postsCount: 0,
+      isActive: true,
+      tags: [],
+      createdAt: now,
+      updatedAt: now,
+    });
+    return topic;
   },
 });
